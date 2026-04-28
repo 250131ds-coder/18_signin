c15d3a3d7c0d2fe4119e5ed74ffdf4ff3cbbca02
250131ds-coder <250131ds@yse-c.net>
2026-04-28 11:50:52 +0900

    first commit

---
+++ signin/auth.php
@@ -0,0 +1,37 @@
+<?php
+// 共通ファイル app.php を読み込み
+require_once '../app.php';
+
+use App\Models\AuthUser;
+use App\Models\User;
+
+// POSTリクエストでなければ何も表示しない
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit;
+}
+
+// TODO: セッションにPOSTデータを登録
+$_SESSION[APP_KEY]['signin'] = sanitize($_POST);
+
+// TODO: 入力されたアカウント名とパスワードを取得
+$account_name = h($_POST['account_name']);
+$password = h($_POST['password']);
+
+// ユーザ認証: new User() で auth() を実行
+$user = new User();
+// アカウント名とパスワードを渡して、認証処理
+$auth_user = $user->auth($account_name, $password);
+
+if (empty($auth_user['id'])) {
+    // エラーセッション
+    $_SESSION[APP_KEY]['error'] = 'アカウント名またはパスワードが間違っています。';
+    // ログイン失敗時はログイン入力画面にリダイレクト
+    header('Location: ./input.php');
+    exit;
+} else {
+    // TODO: 認証成功時はセッションにユーザデータを保存: APP_KEY の signin
+    $_SESSION[APP_KEY]['auth_user'] = $auth_user;
+    // ユーザトップページにリダイレクト: home/
+    header('Location: ../home/');
+    exit;
+}
